7f93c3c49600f6d0ae58a809ed5bb4222c89e685
izharabbasi <izharabbasi103@gmail.com>
2020-09-01 18:31:53 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from post import Post
+
+post = Post('tiele1', 'this is content', 'izhar')
+post2 = Post('tiele2', 'this is different content', 'shery')
+post.author = "ahmed yar"
+print(post.author)
+print(post2.content)
